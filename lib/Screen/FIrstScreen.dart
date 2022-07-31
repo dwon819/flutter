c6ebb99f2d3ebd751ec5eeecdb48426a2e83963a
eumdong-won <dwon819@naver.com>
2022-07-31 18:24:13 +0900

    Count example => State Example

--- lib/Screen/FIrstScreen.dart
+++ lib/Screen/FIrstScreen.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:study_project/Screen/SecondeScreen.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  _FirstScreen createState() => _FirstScreen();
+}
+
+class _FirstScreen extends State<FirstScreen> {
+  int count = 0;
+  void increase() {
+    setState(() {
+      count = count + 1;
+    });
+  }
+
+  void decrease() {
+    setState(() {
+      count = count - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('Count Application'),
+        ),
         body: Center(
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text('This is First Screen'),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (BuildContext context) {
-                  return SecondScreen(
-                    screenData: 'Data from FirstScreen',
-                  );
-                }));
-              },
-              child: Text('Go to Second Screen'))
+          Text(
+            'Count : $count',
+            style: TextStyle(fontSize: 25),
+          ),
+          Padding(padding: EdgeInsets.all(20)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    decrease();
+                  },
+                  child: Text('-1')),
+              ElevatedButton(
+                  onPressed: () {
+                    increase();
+                  },
+                  child: Text('+1')),
+            ],
+          )
         ])));
   }
 }
